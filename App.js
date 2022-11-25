e23887945973887e370b8de7a119b5a1cb67f78c
Anouar Essabbari <89299571+anouaressa@users.noreply.github.com>
2022-11-25 01:58:31 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-const allRonds = document.querySelectorAll('.rond');
-const allBoxes = document.querySelectorAll('.box');
+const allRonds = document.querySelectorAll('.icon');
+const allBoxes = document.querySelectorAll('.content');
 
 const controller = new ScrollMagic.Controller()
 
-allBoxes.forEach(box => {
+allBoxes.forEach(content => {
 
     for(i = 0; i < allRonds.length; i++){
 
-        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+        if(allRonds[i].getAttribute('data-anim') === content.getAttribute('data-anim')){
 
-            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
+            let tween = gsap.from(content, {y: -50, opacity: 0, duration: 0.5})
 
             let scene = new ScrollMagic.Scene({
                 triggerElement: allRonds[i],
